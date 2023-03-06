b62837af8305eb24cd797beda3ecc1a27b117494
Jsrra <josephserrato10@gmail.com>
2023-03-05 18:49:48 -0800

    Adds working menu, schema, and asciiart

--- server.js
+++ server.js
@@ -1,37 +1,37 @@
-const express = require('express');
-// Import and require mysql2
-const mysql = require('mysql2');
+// const express = require('express');
+// // Import and require mysql2
+// const mysql = require('mysql2');
 
-const PORT = process.env.PORT || 3001;
-const app = express();
+// const PORT = process.env.PORT || 3001;
+// const app = express();
 
-// Express middleware
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
+// // Express middleware
+// app.use(express.urlencoded({ extended: false }));
+// app.use(express.json());
 
-// Connect to database
-const db = mysql.createConnection(
-    {
-        host: '123.0.0.1',
-        // MySQL username,
-        user: 'root',
-        // MySQL password
-        password: 'root',
-        database: 'employees_db'
-    },
-    console.log(`Connected to the employees_db database.`)
-);
+// // Connect to database
+// const db = mysql.createConnection(
+//     {
+//         host: '123.0.0.1',
+//         // MySQL username,
+//         user: 'root',
+//         // MySQL password
+//         password: 'root',
+//         database: 'employees_db'
+//     },
+//     console.log(`Connected to the employees_db database.`)
+// );
 
-// Query database
-db.query('SELECT * FROM students', function (err, results) {
-    console.log(results);
-});
+// // Query database
+// db.query('SELECT * FROM students', function (err, results) {
+//     console.log(results);
+// });
 
-// Default response for any other request (Not Found)
-app.use((req, res) => {
-    res.status(404).end();
-});
+// // Default response for any other request (Not Found)
+// app.use((req, res) => {
+//     res.status(404).end();
+// });
 
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
+// app.listen(PORT, () => {
+//     console.log(`Server running on port ${PORT}`);
+// });
